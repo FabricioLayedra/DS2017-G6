8c07c1f81f02f835afde58e8a0c0bdf56742e022
Made Velasco <m_velasco93@hotmail.com>
2017-06-06 19:26:08 -0500

    Initial frontend commit

---
+++ application/controllers/Web.php
@@ -0,0 +1,32 @@
+<?php 
+
+if( !defined('BASEPATH')) exit ("No direct script access allowed");
+
+class Web extends CI_Controller{
+
+	public function __construct(){
+		parent:: __construct();
+		$this->load->helper('url');
+		$this->load->helper('form');
+		$this->load->helper(array('url'));
+		$this->load->library('form_validation');
+		
+		
+		date_default_timezone_set("America/Guayaquil");
+	}
+
+
+	public function index(){
+
+		$dataHeader['PageTitle'] = "";
+
+        $data['header'] = $this->load->view('web/header', $dataHeader);
+        $data['menu'] = $this->load->view('web/menu', array());
+
+        $data['contenido'] = $this->load->view('web/index', array());
+        $data['footer'] = $this->load->view('web/footer', array());
+
+    }
+}
+
+?>
