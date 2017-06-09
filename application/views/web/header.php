ee3e76866b1757efcd1e0675d9e5d1ae91f394a4
Fabricio I. Layedra <flayedra@espol.edu.ec>
2017-06-09 01:45:27 -0500

    Creando página principal

--- application/views/web/header.php
+++ application/views/web/header.php
@@ -14,7 +14,26 @@
 	
 	
 	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/web/bootstrap.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/web/responsive.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/web/bootstrap-tagsinput.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/web/custom-bootstrap-margin-padding.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/web/ejerciciocss.css'); ?>" >
 
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/web/made.css'); ?>" >
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/web/ivan.css'); ?>" >
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/web/icons.css'); ?>" >
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/web/profesorprofile.css'); ?>" >
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/web/errorescss.css'); ?>" >
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+    <!-- Important Owl stylesheet -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/web/owl.carousel.css'); ?>">
+    <!-- Default Theme -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/web/owl.theme.css'); ?>">
 
 </head>
 <body>
