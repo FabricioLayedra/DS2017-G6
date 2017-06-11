1a2470319b1b7108bd839ee7c23ae9a9b9aefcf2
mvelasco93 <m_velasco93@hotmail.com>
2017-06-11 03:13:39 -0500

    Updated inserts

--- sql/db-insert.sql
+++ sql/db-insert.sql
@@ -1,17 +1,10 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.1.23-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             9.4.0.5125
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping data for table ds2017.category: ~2 rows (approximately)
+DELETE FROM `category`;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
 INSERT INTO `category` (`id_category`, `name`) VALUES
 	(1, 'Platos de Mar'),
@@ -19,11 +12,11 @@ INSERT INTO `category` (`id_category`, `name`) VALUES
 	(2, 'Platos típicos');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 
--- Dumping data for table ds2017.dish: ~0 rows (approximately)
+DELETE FROM `dish`;
 /*!40000 ALTER TABLE `dish` DISABLE KEYS */;
 /*!40000 ALTER TABLE `dish` ENABLE KEYS */;
 
--- Dumping data for table ds2017.rbac_group: ~3 rows (approximately)
+DELETE FROM `rbac_group`;
 /*!40000 ALTER TABLE `rbac_group` DISABLE KEYS */;
 INSERT INTO `rbac_group` (`id_group`, `name`) VALUES
 	(1, 'Administrator'),
@@ -31,22 +24,23 @@ INSERT INTO `rbac_group` (`id_group`, `name`) VALUES
 	(3, 'Usuario');
 /*!40000 ALTER TABLE `rbac_group` ENABLE KEYS */;
 
--- Dumping data for table ds2017.rbac_user_group: ~2 rows (approximately)
+DELETE FROM `rbac_user_group`;
 /*!40000 ALTER TABLE `rbac_user_group` DISABLE KEYS */;
 INSERT INTO `rbac_user_group` (`id_user`, `id_group`) VALUES
 	(1, 1),
-	(2, 1);
+	(2, 2),
+	(3, 1);
 /*!40000 ALTER TABLE `rbac_user_group` ENABLE KEYS */;
 
--- Dumping data for table ds2017.restaurant: ~0 rows (approximately)
+DELETE FROM `restaurant`;
 /*!40000 ALTER TABLE `restaurant` DISABLE KEYS */;
 /*!40000 ALTER TABLE `restaurant` ENABLE KEYS */;
 
--- Dumping data for table ds2017.restaurant_assistants: ~0 rows (approximately)
+DELETE FROM `restaurant_assistants`;
 /*!40000 ALTER TABLE `restaurant_assistants` DISABLE KEYS */;
 /*!40000 ALTER TABLE `restaurant_assistants` ENABLE KEYS */;
 
--- Dumping data for table ds2017.type: ~2 rows (approximately)
+DELETE FROM `type`;
 /*!40000 ALTER TABLE `type` DISABLE KEYS */;
 INSERT INTO `type` (`id_type`, `name`) VALUES
 	(1, 'Aperitivo'),
@@ -54,11 +48,12 @@ INSERT INTO `type` (`id_type`, `name`) VALUES
 	(3, 'Postre');
 /*!40000 ALTER TABLE `type` ENABLE KEYS */;
 
--- Dumping data for table ds2017.user: ~2 rows (approximately)
+DELETE FROM `user`;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`id_user`, `name`, `last_name`, `username`, `email`, `password`) VALUES
 	(1, 'Leonardo', 'Kuffo', 'lkuffo', 'lkuffo@espol.edu.ec', '21232f297a57a5a743894a0e4a801fc3'),
-	(2, 'Fabricio', 'Layedra', 'flayedra', 'flayedra@espol.edu.ec', 'a494bfd29b0333678e84861e0bd71c23');
+	(2, 'Fabricio', 'Layedra', 'flayedra', 'flayedra@espol.edu.ec', '21232f297a57a5a743894a0e4a801fc3'),
+	(3, 'Madelyne', 'Velasco', 'mvelasco', 'mvelasco@espol.edu.ec', '21232f297a57a5a743894a0e4a801fc3');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
