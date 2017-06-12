024e7d4d43347ceacd43365bf1fbdd0871abd3a3
mvelasco93 <m_velasco93@hotmail.com>
2017-06-11 20:26:32 -0500

    Updated database

--- sql/db-create.sql
+++ sql/db-create.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `category` (
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_category`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
   `id` varchar(128) NOT NULL,
@@ -27,10 +27,9 @@ CREATE TABLE IF NOT EXISTS `dish` (
   `id_dish` int(11) NOT NULL AUTO_INCREMENT,
   `id_restaurant` int(11) NOT NULL DEFAULT '0',
   `name` varchar(50) DEFAULT NULL,
-  `descripcion` varchar(50) DEFAULT NULL,
+  `descripcion` varchar(300) DEFAULT NULL,
+  `ingredient` varchar(50) DEFAULT NULL,
   `temp` tinyint(4) DEFAULT NULL COMMENT '0: Frio, 1: Templado, 2:Frio',
-  `type` tinyint(4) DEFAULT NULL COMMENT '0: Aperitito, 1:Plato Fuerte, 2:Postre',
-  `published` date NOT NULL,
   `img` varchar(50) DEFAULT NULL,
   `id_category` int(11) NOT NULL,
   `id_type` int(11) NOT NULL,
@@ -41,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `dish` (
   CONSTRAINT `FK_dish_category` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`),
   CONSTRAINT `FK_dish_restaurant` FOREIGN KEY (`id_restaurant`) REFERENCES `restaurant` (`id_restaurant`),
   CONSTRAINT `FK_dish_type` FOREIGN KEY (`id_type`) REFERENCES `type` (`id_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `rbac_group` (
   `id_group` int(11) NOT NULL AUTO_INCREMENT,
@@ -56,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `restaurant` (
   `phone` varchar(50) NOT NULL,
   `owner` varchar(50) NOT NULL,
   PRIMARY KEY (`id_restaurant`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `restaurant_assistants` (
   `id_restaurant` int(11) NOT NULL,
@@ -71,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `type` (
   `id_type` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_type`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
@@ -86,7 +85,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `mail` (`email`),
   KEY `FK_user_rbac_group` (`id_group`),
   CONSTRAINT `FK_user_rbac_group` FOREIGN KEY (`id_group`) REFERENCES `rbac_group` (`id_group`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_assistants` (
+  `id_user` INT(11) NOT NULL,
+  `full_name` VARCHAR(101) NOT NULL COLLATE 'utf8_general_ci'
+) ENGINE=MyISAM;
+
+DROP TABLE IF EXISTS `user_assistants`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `user_assistants` AS SELECT user.id_user, CONCAT(user.name, ' ', user.last_name) AS 'full_name' FROM user WHERE user.id_group = 2 ;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
