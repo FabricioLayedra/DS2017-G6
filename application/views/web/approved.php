b68f566202a9cafb173e1c4cfbac08b4ffa3a99b
Fabricio Layedra <flayedra@espol.edu.ec>
2017-08-16 23:46:44 -0500

    UML Diagrams

--- application/views/web/approved.php
+++ application/views/web/approved.php
@@ -3,10 +3,35 @@
   <div class="container">
     <div class="col-md-4"></div>
     <div class="col-md-4 center">
-    	<p class = 'mt-30' style ="text-align: center;">Su pedido ha sido realizado con éxito. </p>
-      <p class = 'mt-60' style ="color:#FF6A6A;font-size:46px;text-align: center;"> ORDEN No. 25</p>
+    	
     </div>
     <div class="col-md-4"></div>
   </div>
   <div class='pt-30'> </div>
+
+  <!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal"> Malos pagos!</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Pago Exitoso</h4>
+      </div>
+      <div class="modal-body">
+        <p class = 'mt-30' style ="text-align: center;">Su pedido ha sido realizado con éxito. </p>
+      <p class = 'mt-60' style ="color:#FF6A6A;font-size:26px;text-align: center;"> ORDEN No. 25</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Volver</button>
+      </div>
+    </div>
+
+  </div>
+
+
 </div>
