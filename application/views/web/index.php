15d63962b8c528676812f2683c0233011182f325
Madelyne Velasco <m_velasco93@hotmail.com>
2017-08-20 21:20:02 -0500

    WIP: Fix index

--- application/views/web/index.php
+++ application/views/web/index.php
@@ -1,99 +1,99 @@
 <!-- Page Content -->
 <div id="page-wrapper">
-	<div class="row">
-			<div class="col-lg-12"  class="owl-carousel">
 
-
-				<br>
-			  <div id="restosCarousel" class="carousel slide" data-ride="carousel">
-			    <!-- Indicators -->
-			    <ol class="carousel-indicators">
-			      <li data-target="#restosCarousel" data-slide-to="0" class="active"></li>
-			      <li data-target="#restosCarousel" data-slide-to="1"></li>
-			      <li data-target="#restosCarousel" data-slide-to="2"></li>
-			    </ol>
-			    <!-- Wrapper for slides -->
-			    <div class="carousel-inner" role="listbox">
-
-			      <div class="item active">
-			        <img src = "<?php echo base_url('assets/img/slideshow/inicio.png'); ?>" alt="start" >
-			        <div class="carousel-caption">
-			          <h1 style="font-size: 4.5em; ">Bienvenidos a El Comelón!</h1>
-						<p>
-							Servicio de difusión y administración de los Comedores
-							y Bares Politécnicos.
-						</p>
-					</div>
-			      </div>
-
-			      <div class="item">
-			        <img src = "<?php echo base_url('assets/img/slideshow/espol.png'); ?>"  alt="service" >
-			        <div class="carousel-caption">
-								<h1>Bienvenidos a El Comelón!</h1>
-			          <h3>¡Servirte es nuestra recompensa!</h3>
-			          <p>Visítanos en tu facultad.</p>
-			        </div>
-			      </div>
-
-			      <div class="item">
-			        <img src = "<?php echo base_url('assets/img/slideshow/bar-mes.png'); ?>"  alt="barofthemonth" >
-			        <div class="carousel-caption">
-								<h1>Bienvenidos a El Comelón!</h1>
-			          <h3>Bar del Mes</h3>
-			          <p>Frutanga. Patio de Comidas FIMCP.</p>
-			        </div>
-			      </div>
-
-			    </div>
-
-			    <!-- Left and right controls -->
-			    <a class="left carousel-control" href="#restosCarousel" role="button" data-slide="prev">
-			      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			      <span class="sr-only">Previous</span>
-			    </a>
-			    <a class="right carousel-control" href="#restosCarousel" role="button" data-slide="next">
-			      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			      <span class="sr-only">Next</span>
-			    </a>
-			  </div>
-			</div>
-</div>
-</section>
-
-<section id="porque" class="pt-20 pb-50">
-	<div class="container-fluid">
+	<section>
 		<div class="row">
-			<div class="col-lg-12">
-				<h1 class="section2-title ">
-					Pilares de Servicio
-				</h1>
+			<div class="col-md-12  pl-0 pr-0"  class="owl-carousel">
+				<div id="restosCarousel" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#restosCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#restosCarousel" data-slide-to="1"></li>
+						<li data-target="#restosCarousel" data-slide-to="2"></li>
+					</ol>
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner" role="listbox">
+
+						<div class="item active">
+							<img src = "<?php echo base_url('assets/img/slideshow/inicio.png'); ?>" alt="start" >
+							<div class="carousel-caption">
+								<h1 style="font-size: 4.5em; ">Bienvenidos a El Comelón!</h1>
+								<p>
+									Servicio de difusión y administración de los Comedores
+									y Bares Politécnicos.
+								</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<img src = "<?php echo base_url('assets/img/slideshow/espol.png'); ?>"  alt="service" >
+							<div class="carousel-caption">
+								<h1>Bienvenidos a El Comelón!</h1>
+								<h3>¡Servirte es nuestra recompensa!</h3>
+								<p>Visítanos en tu facultad.</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<img src = "<?php echo base_url('assets/img/slideshow/bar-mes.png'); ?>"  alt="barofthemonth" >
+							<div class="carousel-caption">
+								<h1>Bienvenidos a El Comelón!</h1>
+								<h3>Bar del Mes</h3>
+								<p>Frutanga. Patio de Comidas FIMCP.</p>
+							</div>
+						</div>
+
+					</div>
+
+					<!-- Left and right controls -->
+					<a class="left carousel-control" href="#restosCarousel" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#restosCarousel" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
 			</div>
 		</div>
-		<div class="row mt-md-20 pt-10">
-			<div class="col-lg-12">
-				<div class="container-fluid">
-					<div class="row">
-						<div class = 'container'>
-							<div class = 'row pt-15'>
-								<div class = 'col-md-4'>
-									<span class="daw-check section2-icon align-center"></span>
-									<div class="icon-label bold pt-lg-20 align-center">Calidad</div>
-									<div class="description">
-										Cada uno de nuestros bares siempre buscará ofrecer la más alta calidad en la elaboración de sus platillos.
+	
+	</section>
+	<section id="porque" class="pt-20 pb-50">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12">
+					<h1 class="section2-title ">
+						Pilares de Servicio
+					</h1>
+				</div>
+			</div>
+			<div class="row mt-md-20 pt-10">
+				<div class="col-lg-12">
+					<div class="container-fluid">
+						<div class="row">
+							<div class = 'container'>
+								<div class = 'row pt-15'>
+									<div class = 'col-md-4'>
+										<span class="daw-check section2-icon align-center"></span>
+										<div class="icon-label bold pt-lg-20 align-center">Calidad</div>
+										<div class="description">
+											Cada uno de nuestros bares siempre buscará ofrecer la más alta calidad en la elaboración de sus platillos.
+										</div>
 									</div>
-								</div>
-								<div class = 'col-md-4 pt-xs-15'>
-									<span class="daw-device section2-icon align-center"></span>
-									<div class="icon-label bold pt-lg-20 align-center">Portabilidad</div>
-									<div class="description">
-										Puedes revisar que comer y en donde desde cualquier dispositivo.
+									<div class = 'col-md-4 pt-xs-15'>
+										<span class="daw-device section2-icon align-center"></span>
+										<div class="icon-label bold pt-lg-20 align-center">Portabilidad</div>
+										<div class="description">
+											Puedes revisar que comer y en donde desde cualquier dispositivo.
+										</div>
 									</div>
-								</div>
-								<div class = 'col-md-4 pt-xs-15'>
-									<span class="daw-arrow-down section2-icon align-center"></span>
-									<div class="icon-label bold pt-lg-20 align-center">Confianza</div>
-									<div class="description">
-										Cada platillo se prepara bajo una exhaustiva revisión de asepsia para dar a nuestros clientes la tranquilidad de un buen producto.
+									<div class = 'col-md-4 pt-xs-15'>
+										<span class="daw-arrow-down section2-icon align-center"></span>
+										<div class="icon-label bold pt-lg-20 align-center">Confianza</div>
+										<div class="description">
+											Cada platillo se prepara bajo una exhaustiva revisión de asepsia para dar a nuestros clientes la tranquilidad de un buen producto.
+										</div>
 									</div>
 								</div>
 							</div>
@@ -101,87 +101,86 @@
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 
-	<section id = 'citas' class="pt-lg-50 pb-lg-50">
-		<div class = 'container-fluid pt-10'>
-			<div class = 'row mt-lg-40'>
-				<div class = 'col-md-12'>
-					<div id="owl-landing-1" class="owl-carousel">
-						<div class="container-fluid">
-							<div class="row">
-								<div class="col-lg-2 col-lg-offset-1">
-									<img src="<?php echo base_url('assets/img/quotes/quotes-1.png'); ?>" class = 'img-responsive img-circle align-center '>
-								</div>
-								<div class="col-lg-9">
-									<blockquote>
-										<p class="quote">"Los sánduches de Don Germán son la pinta"</p>
-										<footer class="quote-author">Pepe Lucho</footer>
-									</blockquote>
-								</div>
-							</div>
-						</div>
-						<div class="container-fluid">
-							<div class="row">
-								<div class="col-lg-2 col-lg-offset-1">
-									<img src="<?php echo base_url('assets/img/quotes/quotes-2.png'); ?>" class = 'img-responsive img-circle align-center '>
-								</div>
-								<div class="col-lg-9">
-									<blockquote>
-										<p class="quote">"Los pasteles del Chino siempre son una opción de almuerzo."</p>
-										<footer class="quote-author">Galo Castillo</footer>
-									</blockquote>
+		<section id = 'citas' class="pt-lg-50 pb-lg-50">
+			<div class = 'container-fluid pt-10'>
+				<div class = 'row mt-lg-40'>
+					<div class = 'col-md-12'>
+						<div id="owl-landing-1" class="owl-carousel">
+							<div class="container-fluid">
+								<div class="row">
+									<div class="col-lg-2 col-lg-offset-1">
+										<img src="<?php echo base_url('assets/img/quotes/quotes-1.png'); ?>" class = 'img-responsive img-circle align-center '>
+									</div>
+									<div class="col-lg-9">
+										<blockquote>
+											<p class="quote">"Los sánduches de Don Germán son la pinta"</p>
+											<footer class="quote-author">Pepe Lucho</footer>
+										</blockquote>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="container-fluid">
-							<div class="row">
-								<div class="col-lg-2 col-lg-offset-1">
-									<img src="<?php echo base_url('assets/img/quotes/quotes-3.png'); ?>" class = 'img-responsive img-circle align-center '>
-								</div>
-								<div class="col-lg-9">
-									<blockquote>
-										<p class="quote">"Vale la pena hacer fila en FCSH, la comida es deliciosa."</p>
-										<footer class="quote-author">Madelyne Velasco</footer>
-									</blockquote>
-								</div>
-							</div>
-						</div>
-						<div class="container-fluid">
-							<div class="row">
-								<div class="col-lg-2 col-lg-offset-1">
-									<img src="<?php echo base_url('assets/img/quotes/quotes-4.png'); ?>" class = 'img-responsive img-circle align-center '>
-								</div>
-								<div class="col-lg-9">
-									<blockquote>
-										<p class="quote">"!Una empanada de miLatto no basta, sino DOS!"</p>
-										<footer class="quote-author">Viviana Laurido</footer>
-									</blockquote>
+							<div class="container-fluid">
+								<div class="row">
+									<div class="col-lg-2 col-lg-offset-1">
+										<img src="<?php echo base_url('assets/img/quotes/quotes-2.png'); ?>" class = 'img-responsive img-circle align-center '>
+									</div>
+									<div class="col-lg-9">
+										<blockquote>
+											<p class="quote">"Los pasteles del Chino siempre son una opción de almuerzo."</p>
+											<footer class="quote-author">Galo Castillo</footer>
+										</blockquote>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="container-fluid">
-							<div class="row">
-								<div class="col-lg-2 col-lg-offset-1">
-									<img src="<?php echo base_url('assets/img/quotes/quotes-5.png'); ?>" class = 'img-responsive img-circle align-center '>
+							<div class="container-fluid">
+								<div class="row">
+									<div class="col-lg-2 col-lg-offset-1">
+										<img src="<?php echo base_url('assets/img/quotes/quotes-3.png'); ?>" class = 'img-responsive img-circle align-center '>
+									</div>
+									<div class="col-lg-9">
+										<blockquote>
+											<p class="quote">"Vale la pena hacer fila en FCSH, la comida es deliciosa."</p>
+											<footer class="quote-author">Madelyne Velasco</footer>
+										</blockquote>
+									</div>
 								</div>
-								<div class="col-lg-9">
-									<blockquote>
-										<p class="quote">"Si estás de apuro, los americanos de Mecánica son!"</p>
-										<footer class="quote-author">María Belén Guaranda</footer>
-									</blockquote>
+							</div>
+							<div class="container-fluid">
+								<div class="row">
+									<div class="col-lg-2 col-lg-offset-1">
+										<img src="<?php echo base_url('assets/img/quotes/quotes-4.png'); ?>" class = 'img-responsive img-circle align-center '>
+									</div>
+									<div class="col-lg-9">
+										<blockquote>
+											<p class="quote">"!Una empanada de miLatto no basta, sino DOS!"</p>
+											<footer class="quote-author">Viviana Laurido</footer>
+										</blockquote>
+									</div>
+								</div>
+							</div>
+							<div class="container-fluid">
+								<div class="row">
+									<div class="col-lg-2 col-lg-offset-1">
+										<img src="<?php echo base_url('assets/img/quotes/quotes-5.png'); ?>" class = 'img-responsive img-circle align-center '>
+									</div>
+									<div class="col-lg-9">
+										<blockquote>
+											<p class="quote">"Si estás de apuro, los americanos de Mecánica son!"</p>
+											<footer class="quote-author">María Belén Guaranda</footer>
+										</blockquote>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 
 
-    </div>
-    <!-- /.container-fluid -->
+	</div>
+	<!-- /.container-fluid -->
 </div>
 <!-- /#page-wrapper -->
